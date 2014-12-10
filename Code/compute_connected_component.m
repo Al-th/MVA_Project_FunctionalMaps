082f346f7d069b28d5a2b04e6d08bad7b3376fbd
paumiern <paumiern.ensimag@gmail.com>
2014-12-10 19:26:23 +0100

    Segment dectection
    add first version of connected component segmentation

---
+++ Code/compute_connected_component.m
@@ -0,0 +1,79 @@
+function connected_component = compute_connected_component(shape,tau)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+%parameters
+
+num_vertices = size(shape.vertex,1);
+num_faces = size(shape.faces,1);
+C = zeros(num_vertices,1);
+
+% sort vertex w.r.t to WKS
+wks = sum(shape.WKS');
+[~,perm]=sort(wks,'descend');
+perm = perm'; % shape.vertex(perm(1))
+
+for i=1:num_vertices,
+    %process vertex x
+    idx_x = perm(i);
+    x = shape.vertex(idx_x,:);
+    
+    %%
+    %Compute list of vertex in a one-ring neighborhood of x
+    
+    %find faces containing x
+    idx_faces = [];
+    for k=1:3
+        cur_idx = find(shape.faces(:,k)==idx_x);
+        idx_faces = [idx_faces cur_idx'];
+    end
+    % Recupere les vertex de chaque face et supprime les doublons
+    tmp = shape.faces(idx_faces,:);
+    ngb_x_index = intersect(tmp(tmp~=idx_x),tmp(tmp~=idx_x));
+    
+    %%
+    %Determine if x is a local maximum w.r.t wks
+    local_max = wks(idx_x);
+    idx_local_max = idx_x;
+    for k=ngb_x_index',
+        if( wks(k) > local_max)
+            local_max = wks(k);
+            idx_local_max = k;
+        end
+    end
+    if(idx_local_max == idx_x)
+        % Create a new component
+        C(idx_x) = idx_x;
+    else
+        %Add x to the component with the highest wks value
+        C(idx_x) = C(idx_local_max);
+    end
+    
+    %%
+    %If the vertex x is adjacent to two or more existing components,
+    %we check the persistence of the components and merge them
+    %only if they are not tau-persistent
+    nb_adj_component = numel(find(C(ngb_x_index)));
+    a = wks(idx_x);
+    if( nb_adj_component > 1 );
+        
+        x2 = wks(C(idx_x));
+        for k = ngb_x_index'
+            if(C(k) ~= 0)
+                x1 = wks(C(k));
+                if( x1 < x2 )
+                    if( x1 - a <= tau )
+                        %We merge
+                        C(C==C(k)) = C(idx_x);
+                    else
+                        %We don't merge
+                    end
+                end
+            end
+        end
+    end
+end
+
+connected_component = C;
+
+end
+
--- Code/test.m
+++ Code/test.m
@@ -1,4 +1,4 @@
-clear; clc;
+%clear; clc;
 init;
 
 %%
@@ -46,82 +46,14 @@ clear WKS; clear hks;
 clear PHI; clear E; clear L; clear Am;
 clear vertex; clear faces; clear name;
 
-
 %%
-%Compute persistence-based segmentation based on [Skraba et al. 2010]
+%Compute connected component of shape 1
 
-%parameters
-tau = 5;
-num_vertices = size(vertices,1);
-num_faces = size(faces,1);
-C = zeros(num_vertices);
+C = compute_connected_component(shape1,20);
 
-% sort vertex w.r.t to WKS
-wks = sum(shape1.WKS');
-[~,perm]=sort(wks,'descend');
-perm = perm'; % shape1.vertex(perm(1))
-
-for i=1:num_vertices,
-   %process vertex x 
-    idx_x = perm(i);
-    x = shape1.vertex(idx_x,:);
-    
-    %%
-    %Compute list of vertex in a one-ring neighborhood of x
-    
-    %find faces containing x
-    idx_faces = [];
-    for k=1:3      
-       cur_idx = find(shape1.faces(:,k)==idx_x);
-       idx_faces = [idx_faces cur_idx'];       
-    end
-    % Recupere les vertex de chaque face et supprime les doublons
-    tmp = shape1.faces(idx_faces,:);
-    ngb_x_index = intersect(tmp(tmp~=idx_x),tmp(tmp~=idx_x));
-    
-    %%
-    %Determine if x is a local maximum w.r.t wks
-    local_max = wks(idx_x);
-    idx_local_max = idx_x;
-    for k=ngb_x_index,
-        if( wks(k) > local_max)
-            local_max = wks(k);
-            idx_local_max = k;
-        end
-    end
-    if(idx_local_max == idx_x)
-        % Create a new component
-        C(id_x) = idx_x;
-    else
-        %Add x to the component with the highest wks value
-        C(id_x) = C(idx_local_max);
-    end
-    
-    %%
-    %If the vertex x is adjacent to two or more existing components, 
-    %we check the persistence of the components and merge them 
-    %only if they are not tau-persistent
-    nb_adj_component = numel(find(C(ngb_x_index)));
-    a = wks(idx_x);
-    if( nb_adj_component > 1 );
-        
-        x2 = wks(C(idx_x));
-        
-        for k = ngb_x_index
-            x1 = wks(C(k));
-            if( x1 < x2 )
-                if( x1 - a <= tau )
-                    %We merge
-                    C(C==C(k)) = C(idx_x);
-                else
-                    %We don't merge
-                end
-            end
-        end
-    end
-end
-
-
+options.face_vertex_color = C;
+plot_mesh(shape1.vertex,shape1.faces,options);
+shading interp; colormap jet(256);
 
 
 
