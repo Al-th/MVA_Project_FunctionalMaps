f59bfca06760bbb130e7255e3ca84cdfacf0ef56
Alexandre This <alexandre.this@gmail.com>
2014-12-10 18:16:12 +0100

    added some code for kdtree

--- Code/test_alex.m
+++ Code/test_alex.m
@@ -1,6 +1,8 @@
 clear; clc;
 init;
+%%
 
+alboptions.n_eigenvalues = 2; 
 %%
 %load shape 1
 name = 'Data/shrec10/0003.null.0.off';
@@ -10,7 +12,7 @@ vertex = vertex'; faces = faces';
 shape1.vertex = vertex;
 shape1.faces = faces;
 %Compute ALB
-[PHI,E,L,Am] = ALB_spectrum(vertex,faces);
+[PHI,E,L,Am] = ALB_spectrum(vertex,faces,alboptions);
 shape1.phi = PHI;
 shape1.E = E;
 shape1.Am = Am;
@@ -18,7 +20,7 @@ shape1.Am = Am;
 hks = HKS(PHI, E, diag(Am), false);
 shape1.HKS = hks;
 %Compute WKS
-WKS = compute_WKS_from_EV(E,PHI);
+WKS = compute_WKS_from_EV(E,PHI,alboptions);
 shape1.WKS = WKS;
 clear WKS; clear hks;
 clear PHI; clear E; clear L; clear Am;
@@ -32,7 +34,7 @@ shape2.name = name;
 vertex = vertex'; faces = faces';
 shape2.vertex = vertex;
 shape2.faces = faces;
-[PHI,E,L,Am] = ALB_spectrum(vertex,faces);
+[PHI,E,L,Am] = ALB_spectrum(vertex,faces,alboptions);
 shape2.phi = PHI;
 shape2.E = E;
 shape2.Am = Am;
@@ -40,7 +42,7 @@ shape2.Am = Am;
 hks = HKS(PHI, E, diag(Am), false);
 shape2.HKS = hks;
 %Compute WKS
-WKS = compute_WKS_from_EV(E,PHI);
+WKS = compute_WKS_from_EV(E,PHI,alboptions);
 shape2.WKS = WKS;
 clear WKS; clear hks;
 clear PHI; clear E; clear L; clear Am;
@@ -59,10 +61,45 @@ b = [b1 b2];
 C = b/a;
 %%
 %Build KD-tree
-tree = kdtree_build(shape2.phi);
+tree1 = kdtree_build(shape1.phi);
+tree2 = kdtree_build(shape2.phi);
+
 %%
-p1 = shape1.phi(1,:)';
-nn = kdtree_k_nearest_neighbors(tree,C*p1,1);
+clear options
+colors = zeros(19248,1);
+colors(1:1000,1) = 2;
+options.face_vertex_color = colors
+plot_mesh(shape1.vertex,shape1.faces,options);
+shading interp; colormap jet(256);
+
+%%
+%Search for 100 first vertex and set colors 
+clear options2;
+color = zeros(19248,1);
+for i = 1:1000
+    p1 = shape1.phi(i,:)';
+    nn = kdtree_k_nearest_neighbors(tree2,C*p1,1);
+    [fi,fj] = find(shape2.faces==nn);
+    color(shape2.faces(fi,1)) = 2;
+    color(shape2.faces(fi,2)) = 2;
+    color(shape2.faces(fi,3)) = 2;
+end
+options2.face_vertex_color = color;
+plot_mesh(shape2.vertex,shape2.faces,options2);
+shading interp; colormap jet(256);
+
+%%
+%Refinement of C
+clc
+C0 = C;
 %%
 
-
+C0P1 = (C0*shape1.phi')';
+s = 0;
+for i = 1:19248
+    pSearch = C0P1(i,:);
+    nn = kdtree_k_nearest_neighbors(tree2,pSearch,1);
+    nnVal = shape2.phi(nn,:);
+    s=s+norm(pSearch-nnVal);
+end
+s
