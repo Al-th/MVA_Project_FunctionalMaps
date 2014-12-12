7b85e7bb2e389bb2176ffc74aef8c782791dfebb
Alexandre This <alexandre.this@gmail.com>
2014-12-12 17:56:11 +0100

    Added some debugging and solver(2)

--- Code/test_alex.m
+++ Code/test_alex.m
@@ -140,31 +140,50 @@ end
 A = [];
 b = [];
 
+
+disp('Adding HKS preservation constraints')
+tic
 %%add descriptor preservation constraints
 for i = 1:size(shape1.HKS,2)
     [dA,db] = computeDescriptorConstraints(shape1.HKS(:,i),shape1,shape2.HKS(:,i),shape2);
     A = sparse([A ; dA]);
     b = sparse([b ; db]);
 end
+toc
 
-%%
+disp('Adding WKS preservation constraints')
+tic
 for i = 1:size(shape1.WKS,2)
     [dA,db] = computeDescriptorConstraints(shape1.WKS(:,i),shape1,shape2.WKS(:,i),shape2);
     A = sparse([A ; dA]);
     b = sparse([b ; db]);
 end
+toc
 
+disp('Adding HKS operator commutativity constraints')
+tic
 %%add operator commutativity constraints
 for i = 1:size(shape1.HKS,2)
     [dA,db] = computeOperatorCommutativityConstraints(shape1.HKS(:,i),shape1,shape2);
     A = sparse([A; dA]);
     b = sparse([b; db]);
 end
+toc
 
+disp('Adding WKS operator commutativity constraints')
+tic
 for i = 1:size(shape1.WKS,2)
     [dA,db] = computeOperatorCommutativityConstraints(shape1.WKS(:,i),shape1,shape2);
     A = sparse([A; dA]);
     b = sparse([b; db]);
 end
+toc
 
-
+%%
+%Solve linear system
+disp('Solving linear system Ax=b')
+tic
+x = mldivide(A,b);
+toc
+%
+C = reshape(x,100,100)';
